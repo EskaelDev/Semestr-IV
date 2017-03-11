404499d087d397d04d164578508f2d04be670662
Tomek Demczuk <tomekkomek@gmail.com>
2017-03-11 17:11:15 +0100

    Java -Okrety -Dzialaja

--- PW/PW_03_HermetyzacjaDziedziczenie_Okret/src/Main.java
+++ PW/PW_03_HermetyzacjaDziedziczenie_Okret/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        OkretPodwodny nazwa = new OkretPodwodny("Kozak", 100, 200, 20);
+        nazwa.Wyswietl();
+        nazwa.zanurz(20);
+        System.out.println("Zanurzenie: " + nazwa.Zanurzenie());
+
+        System.out.println();
+
+        OkretNawodny nazwa2 = new OkretNawodny("Hej", 100, 200, 12);
+        nazwa2.Wyswietl();
+
+        if (nazwa2.CzyZakotwiczony()) {
+            System.out.println("Okret zakotwiczony");
+        }
+        nazwa2.RzucKotwice();
+
+        if (nazwa2.CzyZakotwiczony()) {
+            System.out.println("Okret zakotwiczony");
+        }
+        nazwa2.PodniesKotwice();
+        if (nazwa2.CzyZakotwiczony()) {
+            System.out.println("Okret zakotwiczony");
+        }
     }
 }
